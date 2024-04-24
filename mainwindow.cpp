65535cdf2ed3bee7909439270a169f6c977163ea
saidhann <saidhnn@gmail.com>
2024-04-24 04:15:59 +0200

    Added MainWindow, basic grafics and interative buttons.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,15 +5,114 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
     ui->setupUi(this);
-    QPixmap backGround("C:\\Users\\saidh\\OneDrive\\Pulpit\\QtProject\\VisualisationProject\\background.png");
-    backGround = backGround.scaled(this->size(),Qt::IgnoreAspectRatio);
+    /*
+    setAttribute(Qt::WA_StyledBackground, true);
+    QPixmap backgroundImage(":/newBackground.png");
     QPalette palette;
-    palette.setBrush(QPalette::Window,backGround);
+    palette.setBrush(QPalette::Window, backgroundImage.scaled(size())); // Scale image to fit window size
+    setPalette(palette);
     this->setPalette(palette);
+    */
+    //palette.setBrush(QPalette::Window, Qt::transparent);
+
+    //this->setAttribute(Qt::WA_TranslucentBackground, true);
+    //this->setStyleSheet("background: transparent;");
+    //this->setAutoFillBackground(true);
+
+
+
+    // Set the size policy of the label to expand and maintain aspect ratio;
+
+    // Set layout to central widget
+
+
+    // Create a label to hold the background image
+    setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+    //setAttribute(Qt::WA_TranslucentBackground);
+    setMouseTracking(true);
+
+
+    ui->LivingroomTermometer->hide();
+    ui->BathroomTermometer->hide();
+    ui->UpstairsBigBedroomTermometer->hide();
+    ui->UpstairsSmallBedroomTermometer->hide();
+}
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        // Save the click position relative to the window position
+        dragStartPosition = event->globalPosition().toPoint() - frameGeometry().topLeft();
+        event->accept();
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        // Move the window according to the click position
+        move(event->globalPosition().toPoint() - dragStartPosition);
+        event->accept();
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_LivingroomButton_clicked()
+{
+    ui->LivingroomTermometer->setHidden(false);
+}
+
+
+void MainWindow::on_TurnoffButton_clicked()
+{
+    ui->LivingroomTermometer->hide();
+}
+
+void MainWindow::on_BathroomButton_clicked()
+{
+    ui->BathroomTermometer->setHidden(false);
+}
+
+
+void MainWindow::on_TurnoffButton_2_clicked()
+{
+    ui->BathroomTermometer->hide();
+}
+
+
+void MainWindow::on_UpstairsBigBedroomButton_clicked()
+{
+    ui->UpstairsBigBedroomTermometer->setHidden(false);
+}
+
+
+void MainWindow::on_TurnoffButton_3_clicked()
+{
+    ui->UpstairsBigBedroomTermometer->hide();
+}
+
+
+void MainWindow::on_UpstairsSmallBedroomButton_clicked()
+{
+    ui->UpstairsSmallBedroomTermometer->setHidden(false);
+}
+
+
+void MainWindow::on_TurnoffButton_4_clicked()
+{
+    ui->UpstairsSmallBedroomTermometer->hide();
+}
+
+
+void MainWindow::on_ShutDownButton_clicked()
+{
+    this->close();
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPixmap>
+#include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QtWidgets>
+#include <QVBoxLayout>
+#include <QMouseEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +24,32 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+
+private slots:
+    void on_LivingroomButton_clicked();
+
+    void on_TurnoffButton_clicked();
+
+    void on_BathroomButton_clicked();
+
+    void on_TurnoffButton_2_clicked();
+
+    void on_UpstairsBigBedroomButton_clicked();
+
+    void on_TurnoffButton_3_clicked();
+
+    void on_UpstairsSmallBedroomButton_clicked();
+
+    void on_TurnoffButton_4_clicked();
+
+    void on_ShutDownButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QPoint dragStartPosition;
+
 };
 #endif // MAINWINDOW_H
